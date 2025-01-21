f26edb3d3af88a383dfd6f146d649874ad42396e
ws <wangshuai13368@163.com>
2025-01-21 23:19:48 +0800

    feat:init

--- lib/pages/near/near_view.dart
+++ lib/pages/near/near_view.dart
@@ -1,9 +1,11 @@
-import 'package:easy_refresh/easy_refresh.dart';
+import 'package:chip_list/chip_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:openim/pages/near/near_logic.dart';
 import 'package:openim_common/openim_common.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NearPage extends StatelessWidget {
   final logic = Get.find<NearLogic>();
@@ -14,8 +16,8 @@ class NearPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-    final u = UserInfo();
-    List<UserInfo> items = [u];
+    final u = UserFullInfo();
+    List<UserFullInfo> items = [u];
 
     return Scaffold(
         appBar: AppBar(
@@ -30,22 +32,99 @@ class NearPage extends StatelessWidget {
             onLoading: logic.onLoading,
             controller: logic.refreshController,
             child: ListView.builder(
-              itemBuilder: (c, i) => items[i],
+              itemBuilder: (c, i) => _buildUserListView(items[i]),
               itemExtent: 100.0,
               itemCount: items.length,
             ),
           ))
         ]));
   }
-}
 
-class UserInfo extends StatelessWidget {
-  UserInfo({super.key});
+  Widget _buildUserListView(UserFullInfo user) => Slidable(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Row(
+            children: [
+              AvatarView(
+                width: 52.w,
+                height: 52.h,
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                enabledPreview: true,
+                url:
+                    "https://tse3-mm.cn.bing.net/th/id/OIP-C.msFXsoMqy2Xm9uV4O5ZFiAHaJQ?w=201&h=251&c=7&r=0&o=5&pid=1.7",
+              ),
+              SizedBox(width: 10.w),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "张三身上",
+                    style: TextStyle(fontSize: 16.sp),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 3,
+                            ),
+                            child: Text(
+                              '健身',
+                              style: TextStyle(
+                                  fontSize: 12.sp, color: Colors.white),
+                            )),
+                        margin: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                              left: Radius.circular(5),
+                              right: Radius.circular(5)),
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 3,
+                            ),
+                            child: Text(
+                              '北京',
+                              style: TextStyle(
+                                  fontSize: 12.sp, color: Colors.white),
+                            )),
+                        margin: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                              left: Radius.circular(5),
+                              right: Radius.circular(5)),
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  )
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [Text("user1")],
-    );
-  }
+                  // Row(
+                  //   children: [
+
+                  //     Chip(
+                  //       label: Text(
+                  //         "健身",
+                  //         style: TextStyle(fontSize: 14.sp),
+                  //       ),
+                  //       padding: EdgeInsets.symmetric(
+                  //           horizontal: 0.1.h, vertical: 0.1.w),
+                  //     )
+                  //   ],
+                  // )
+                ],
+              ),
+              Spacer(),
+              Center(
+                child: Text('2.68km | 1小时前'),
+              )
+            ],
+          ),
+        ),
+      );
+  // Widget _buildUserInfoView()=>Slidable(child:Text('view') ,);
 }
