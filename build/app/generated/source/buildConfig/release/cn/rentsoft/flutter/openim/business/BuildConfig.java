bc8d45e7c3b6a4589ffb2f69924871bc07b763ca
bearxiao9756 <bearxiao9@gmail.com>
2025-08-30 20:01:23 +0800

    B5

---
+++ build/app/generated/source/buildConfig/release/cn/rentsoft/flutter/openim/business/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.rentsoft.flutter.openim.business;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "io.openim.flutter.demo";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 180;
+  public static final String VERSION_NAME = "3.8.3";
+}
